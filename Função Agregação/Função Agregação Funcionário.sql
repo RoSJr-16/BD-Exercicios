6193ac1c2ee129016d3fb7ab3f55a87cebbd3739
RoSJr-16 <irodlfnathjr15@gmail.com>
2024-11-18 08:35:22 -0300

    Add files via upload

--- Função Agregação/Função Agregação Funcionário.sql
+++ Função Agregação/Função Agregação Funcionário.sql
@@ -5,7 +5,7 @@ drop table tbFunc
 create table tbFunc(
 codfunc int not null,
 Nome_func varchar(100) not null,
-Uf char not null,
+Uf varchar(2) not null,
 Idade int not null,
 Qtde_filhos int not null,
 primary key (Codfunc)
@@ -16,10 +16,10 @@ drop table tbFuncional
 create table tbFuncional(
 Codfuncional int not null,
 Cargo varchar(100) not null,
-Salário Money not null,
+Salário DECIMAL(10, 3) not null,
 Tempo_empresa int not null,
 Tempo_cargo int not null,
-Sindicalizado char not null,
+Sindicalizado varchar (4) not null,
 codfunc int not null,
 primary key(Codfuncional),
 FOREIGN KEY (codfunc) REFERENCES tbFunc(codfunc)
@@ -39,15 +39,29 @@ insert into tbFunc
 (10, 'Beatriz Pereira', 'MS', 31, 1);
 
 insert into tbFuncional
-(Codfuncional, Cargo, Salário, Tempo_empresa, Tempo_cargo, Sindicalizado)
-(100, ),
-(101, ),
-(102, ),
-(103, ),
-(104, ),
-(105, ),
-(106, ),
-(107, ),
-(108, ),
-(109, ),
+(Codfuncional, Cargo, Salário, Tempo_empresa, Tempo_cargo, Sindicalizado, codfunc) values
+(100, 'Diretor de Projeto', 3.701, 1, 1, 'Sim', 1),
+(101, 'Designer de Jogabilidade', 3.245, 10, 3, 'Não', 2),
+(102, 'Desenvolvedor de Engine', 5.167, 21, 8, 'Sim', 3),
+(103, 'Artista Conceitual', 4.792, 1, 1, 'Sim', 4),
+(104, 'Programador de Jogabilidade', 3.265, 11, 4, 'Não', 5),
+(105, 'Designer de Niveis', 3.067, 2, 2, 'Não', 6),
+(106, 'Animador 3D', 4.625, 12, 10, 'Sim', 7),
+(107, 'Testador de Qualidade', 1.661, 5, 2, 'Não', 8),
+(108, 'Produtor Executivo', 10.667, 10, 9, 'Não', 9),
+(109, 'Especialista em Efeitos Visuais', 9.545, 9, 8, 'Sim', 10);
+
+-- APRESENTAR
+select * from tbFunc
+select * from tbFuncional
+
+-- SOMATÓRIA (1 - 6)
+select sum(codfunc) from tbFunc;
+select sum(Idade) from tbFunc;
+select sum(Qtde_filhos) from tbFunc;
+select sum(Salário) from tbFuncional;
+select sum(Tempo_empresa) from tbFuncional;
+select sum(Tempo_cargo) from tbFuncional;
+
+-- MAIOR E MENOR (7 - 15)
 
